7b138368add11a739090a29fb9ed4c895cb697cc
[Pratiksha] <[mishra.pratiksha1002@gmail.com]>
2023-08-29 09:37:11 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import Login from "./components/user/login";
+function App() {
+  return (
+    <div className="App" style={{ marginTop: 64 }}>
+      <Login></Login>
+    </div>
+  );
+}
+
+export default App;
